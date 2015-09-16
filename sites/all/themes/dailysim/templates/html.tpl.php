1e9205b863792e764053f555b6f8397a45b5ca42
hoqkhanh@gmail.com <Hoquangkhanh761265>
2015-09-16 23:08:44 +0700

    update code

--- sites/all/themes/dailysim/templates/html.tpl.php
+++ sites/all/themes/dailysim/templates/html.tpl.php
@@ -49,6 +49,12 @@
 <head profile="<?php print $grddl_profile; ?>">
     <?php print $head; ?>
     <title><?php print $head_title; ?></title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <?php print $styles; ?>
     <?php print $scripts; ?>
 </head>
--- sites/all/themes/dailysim/templates/page/page.tpl.php
+++ sites/all/themes/dailysim/templates/page/page.tpl.php
@@ -1,45 +1,59 @@
 <?php
 ?>
-  <?php print render($page['header']); ?>
+<?php print render($page['header']); ?>
 
-  <div id="wrapper">
+<div id="wrapper">
     <div id="container" class="clearfix">
 
-      <div id="header">
-      </div> <!-- /#header -->
-
-      <?php if ($page['left']): ?>
-        <div id="sidebar-first" class="sidebar">
-          <?php print render($page['left']); ?>
+        <div id="header">
+            <div id="header-content">
+                <div id="logo"><span class="top">Đại lý sim</span><span class="bottom">.com.vn</span></div>
+                <div id="search-top">
+                    <form role="search" method="get" id="searchform" class="searchform" action="http://demo2.woothemes.com/wootique">
+                        <label class="screen-reader-text" for="s">Search for:</label>
+                        <input type="text" value="" name="s" id="s" class="field s" placeholder="Tìm kiếm sim">
+                        <input type="image" class="submit btn" name="submit" value="Search" src="http://demo2.woothemes.com/wootique/wp-content/themes/wootique/images/ico-search.png">
+                        <input type="hidden" name="post_type" value="product">		</form>
+                    <div class="fix"></div>
+                </div>
+            </div>
         </div>
-      <?php endif; ?>
 
-      <div id="center"><div id="squeeze"><div class="right-corner"><div class="left-corner">
-          <?php print $breadcrumb; ?>
-          <a id="main-content"></a>
-          <?php if ($tabs): ?><div id="tabs-wrapper" class="clearfix"><?php endif; ?>
-          <?php print render($title_prefix); ?>
-          <?php if ($title): ?>
-            <h1 style="display: none" <?php print $tabs ? ' class="with-tabs"' : '' ?>><?php print $title ?></h1>
-          <?php endif; ?>
-          <?php print render($title_suffix); ?>
-          <?php if ($tabs): ?><?php print render($tabs); ?></div><?php endif; ?>
-          <?php print render($tabs2); ?>
-          <?php print $messages; ?>
-          <?php print render($page['help']); ?>
-          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-          <div class="clearfix">
-            <?php print render($page['content']); ?>
-          </div>
-          <?php print $feed_icons ?>
-          <?php print render($page['footer']); ?>
-      </div></div></div></div> <!-- /.left-corner, /.right-corner, /#squeeze, /#center -->
+        <div id="center">
+            <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
+            <?php print $breadcrumb; ?>
+            <?php if ($tabs): ?><div id="tabs-wrapper" class="clearfix"><?php endif; ?>
+                <?php print render($title_prefix); ?>
+                <?php print render($title_suffix); ?>
+                <?php if ($tabs): ?><?php print render($tabs); ?></div><?php endif; ?>
+            <?php print render($tabs2); ?>
+            <?php print $messages; ?>
+            <?php print render($page['help']); ?>
+            <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+            <div id="content">
+                <?php if ($page['left']): ?>
+                    <div id="sidebar-first" class="sidebar">
+                        <?php print render($page['left']); ?>
+                    </div>
+                <?php endif; ?>
+                <div class="main">
+                    <div class="clearfix">
+                        <?php print render($page['content']); ?>
+                    </div>
+                </div>
+                <?php if ($page['right']): ?>
+                    <div id="sidebar-second" class="sidebar">
+                        <?php print render($page['right']); ?>
+                    </div>
+                <?php endif; ?>
+                <?php print $feed_icons ?>
+            </div>
+            <div id="footer">
+                <p class="left">Copyright 2015@ dailysim.com.vn</p>
+                <p class="right">Hotline: 0995 888 999 </p>
+            </div>
+        </div> <!-- /.left-corner, /.right-corner, /#squeeze, /#center -->
 
-      <?php if ($page['right']): ?>
-        <div id="sidebar-second" class="sidebar">
-          <?php print render($page['right']); ?>
-        </div>
-      <?php endif; ?>
 
     </div> <!-- /#container -->
-  </div> <!-- /#wrapper -->
+</div> <!-- /#wrapper -->
