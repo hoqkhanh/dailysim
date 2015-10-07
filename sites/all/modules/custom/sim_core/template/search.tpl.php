11c950d119e7e3ae15813f1e3dc60b73df9236ad
hoqkhanh@gmail.com <Hoquangkhanh761265>
2015-10-07 23:29:49 +0700

    handle search logic

--- sites/all/modules/custom/sim_core/template/search.tpl.php
+++ sites/all/modules/custom/sim_core/template/search.tpl.php
@@ -6,23 +6,35 @@
             <div class="col-md-4">
                 <select name="mang" class="form-control input-sm strong fixprice">
                     <option value="All">Chọn mạng</option>
-                    <option value="27">Viettel</option>
-                    <option value="26">Mobifone</option>
-                    <option value="25">Vinaphone</option>
-                    <option value="28">Vietnamobile</option>
-                    <option value="29">Gmobile</option>
+
+                    <?php
+                        $select_data = array(
+                            array('value' => 27, 'key' => "Viettel"),
+                            array('value' => 26, 'key' => "Mobifone"),
+                            array('value' => 25, 'key' => "Vinaphone"),
+                            array('value' => 28, 'key' => "Vietnamobile"),
+                            array('value' => 29, 'key' => "Gmobile"),
+                        )
+                    ?>
+                    <?php foreach($select_data as $select):?>
+                        <?php if( isset($_GET['mang']) && $_GET['mang'] == $select['value']):?>
+                            <option selected value="<?php echo $select['value'];?>"><?php echo $select['key'];?></option>
+                        <?php else:?>
+                            <option value="<?php echo $select['value'];?>"><?php echo $select['key'];?></option>
+                        <?php endif;?>
+                    <?php endforeach;?>
                 </select>
             </div>
             <div class="col-md-4">
                 <div class="input-group fixprice">
                     <div class="input-group-addon font-12 strong"> Giá từ:</div>
-                    <input name="gia[min]" class="form-control input-sm price" placeholder="100,000">
+                    <input name="gia[min]" value="<?php echo !empty($_GET['gia']['min'])?$_GET['gia']['min']:"";?>" class="form-control input-sm price" placeholder="100,000">
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="input-group fixprice">
                     <div class="input-group-addon font-12 strong">Đến:</div>
-                    <input name="gia[max]" class="form-control input-sm price" placeholder="1,000,000">
+                    <input name="gia[max]" value="<?php echo !empty($_GET['gia']['max'])?$_GET['gia']['max']:"";?>" class="form-control input-sm price" placeholder="1,000,000">
                 </div>
             </div>
             <div class="clearfix"></div>
@@ -32,7 +44,7 @@
             <div class="clearfix"></div>
             <div class="text-center">
                 <div class="col-md-8 col-md-offset-1">
-                    <input name="so" id="sim" value="" class="form-control popover-dismiss" placeholder="Nhập số sim bạn cần tìm" data-container="body" data-toggle="popover" data-placement="bottom" data-content=" - Sử dụng dấu <span class='red'>x</span> đại điện cho 1 số và dấu <span class='red'>*</span> đại điện cho một chuỗi số. <br /> + Để tìm sim bắt đầu bằng 098, quý khách nhập vào 098*<br /> + Để tìm sim kết thúc bằng 888, quý khách nhập vào *888<br /> + Để tìm sim bắt đầu bằng 098 và kết thúc bằng 888, nhập vào 098*888<br /> + Để tìm sim bên trong có số 888, nhập vào 888<br /> + Để tìm sim bắt đầu bằng 098 và kết thúc bằng 808, 818, 828, 838, 848, 858, 868, 878, 888, 898 nhập vào 098*8x8<br /> " title="" data-original-title="Hướng dẫn tìm kiếm sim">
+                    <input name="so" id="sim" value="<?php echo !empty($_GET['so'])?$_GET['so']:"";?>" class="form-control popover-dismiss" placeholder="Nhập số sim bạn cần tìm" data-container="body" data-toggle="popover" data-placement="bottom" data-content=" - Sử dụng dấu <span class='red'>x</span> đại điện cho 1 số và dấu <span class='red'>*</span> đại điện cho một chuỗi số. <br /> + Để tìm sim bắt đầu bằng 098, quý khách nhập vào 098*<br /> + Để tìm sim kết thúc bằng 888, quý khách nhập vào *888<br /> + Để tìm sim bắt đầu bằng 098 và kết thúc bằng 888, nhập vào 098*888<br /> + Để tìm sim bên trong có số 888, nhập vào 888<br /> + Để tìm sim bắt đầu bằng 098 và kết thúc bằng 808, 818, 828, 838, 848, 858, 868, 878, 888, 898 nhập vào 098*8x8<br /> " title="" data-original-title="Hướng dẫn tìm kiếm sim">
 <!--                    <input type="hidden" name="so_op" value="contains">-->
                 </div>
                 <div class="col-md-offset-1"><button class="tim btn btn-info">Tìm sim</button></div>
---
+++ sites/all/themes/dailysim/templates/page/page--search.tpl.php
@@ -0,0 +1,60 @@
+<?php
+?>
+<?php print render($page['header']); ?>
+
+<div id="wrapper">
+    <div id="container" class="clearfix">
+
+        <div id="header">
+            <div id="header-content">
+                <div id="logo"><span class="top">Đại lý sim</span><span class="bottom">.com.vn</span></div>
+                <div id="search-top">
+                    <img src="<?php echo base_path().path_to_theme().'/images/logo.png';?>" />
+                </div>
+            </div>
+        </div>
+
+        <div id="center">
+            <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
+            <?php if ($tabs): ?><div id="tabs-wrapper" class="clearfix"><?php endif; ?>
+                <?php print render($title_prefix); ?>
+                <?php print render($title_suffix); ?>
+                <?php if ($tabs): ?><?php print render($tabs); ?></div><?php endif; ?>
+            <?php print render($tabs2); ?>
+            <?php print $messages; ?>
+            <?php print render($page['help']); ?>
+            <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+            <div id="content">
+                <?php if ($page['left']): ?>
+                    <div id="sidebar-first" class="sidebar">
+                        <?php print render($page['left']); ?>
+                    </div>
+                <?php endif; ?>
+                <div class="main">
+                    <div class="clearfix">
+                        <div id="search">
+                            <h2>Công cụ tìm kiếm</h2>
+                            <?php
+                            $block = module_invoke('simcore', 'block_view', 'search_sim');
+                            print render($block['content']);
+                            ?>
+                        </div>
+                        <?php print render($page['content']); ?>
+                    </div>
+                </div>
+                <?php if ($page['right']): ?>
+                    <div id="sidebar-second" class="sidebar">
+                        <?php print render($page['right']); ?>
+                    </div>
+                <?php endif; ?>
+                <?php print $feed_icons ?>
+            </div>
+            <div id="footer">
+                <p class="left">Copyright 2015@ dailysim.com.vn</p>
+                <p class="right">Hotline: 0995 888 999 </p>
+            </div>
+        </div> <!-- /.left-corner, /.right-corner, /#squeeze, /#center -->
+
+
+    </div> <!-- /#container -->
+</div> <!-- /#wrapper -->
